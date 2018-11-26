9563cd6fc520206e13f4dd4ffc8af1dab46fb539
soe-j <soeda@val.co.jp>
2018-11-26 21:55:58 +0900

    何も選択していないとき、URLをdecodeしてcopy

--- javascripts/content-script.js
+++ javascripts/content-script.js
@@ -1,4 +1,13 @@
-console.log('hoge');
 document.addEventListener('copy', function (e) {
-  console.log('fuga');
+
+  // 文字を選択している
+  if (window.getSelection().toString()) return;
+
+  // フォーム内で文字を選択している
+  if (document.activeElement.selectionStart !== document.activeElement.selectionEnd) return;
+
+  var url = decodeURIComponent(location.href);
+  e.clipboardData.setData("text/plain", url);
+  e.preventDefault(); // もとのcopy処理を実行しない
+
 });
