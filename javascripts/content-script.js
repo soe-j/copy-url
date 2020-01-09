fe3d7ecf9012b284ea20fc93ff9c72595f809c2c
soe-j <soeda@val.co.jp>
2020-01-09 14:29:42 +0900

    slackなどで、以下の文字列でURLが区切られてしまうので

--- javascripts/content-script.js
+++ javascripts/content-script.js
@@ -6,7 +6,11 @@ document.addEventListener('copy', function (e) {
   // フォーム内で文字を選択している
   if (document.activeElement.selectionStart !== document.activeElement.selectionEnd) return;
 
-  var url = decodeURIComponent(location.href);
+  const decodedUrl = decodeURIComponent(location.href);
+
+  // slackなどで、以下の文字列でURLが区切られてしまうので
+  const url = decodedUrl.replace(/\ /g, '%20').replace(/　/g, '%E3%80%80').replace(/／/g, '%EF%BC%8F');
+
   e.clipboardData.setData("text/plain", url);
   e.preventDefault(); // もとのcopy処理を実行しない
 
